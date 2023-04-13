ab9935e6fa0bccb32333a78c3b5e0487b859aa12
Murli Manohar Thakur <mthakur@ishir.com>
2023-04-13 20:14:30 +0530

    Authentication and Authorization in .NET 6 with JWT and ASP.Net Identity

---
+++ StudentTeacher.Core/Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentTeacher.Core.Models
+{
+    public class Teacher
+    {
+
+        [Column("TeacherId")]
+        public int Id { get; set; }
+
+        [Column("Email"), MaxLength(30)]
+        public string? Email { get; set;}
+
+
+        [Required(ErrorMessage = "Teacher's name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+        public string? Name { get; set; }
+
+        public string? Subject { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+    }
+}
---
+++ StudentTeacher.Repo/Migrations/20230413142333_add-gmail-teacher.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentTeacher.Repo.Migrations
+{
+    public partial class addgmailteacher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Teachers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+    }
+}
